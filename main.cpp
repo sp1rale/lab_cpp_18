b8a98e3d889207b335d269f92b14026a5fc410bf
sp1rale <dimayaremchyk24@gmail.com>
2023-12-03 19:57:44 +0200

    done

--- main.cpp
+++ main.cpp
@@ -3,11 +3,32 @@
 
 using namespace std;
 
+struct Boiler {
+	string brand;
+	string color;
+	int power;
+	double volume;
+	int heatingTemperature;
+};
+
+
+
 int main()
 {
-	string name;
-	cout << "Enter your name: ";
-	cin >> name;
-	cout << "Hello, " << name << "!\n";
+	Boiler myBoiler;
+
+	myBoiler.brand = "Bosh";
+	myBoiler.color = "White";
+	myBoiler.power = 2000;
+	myBoiler.volume = 50.0;
+	myBoiler.heatingTemperature = 80;
+
+	cout << "Boiler Information:" << endl;
+	cout << "Brand: " << myBoiler.brand << endl;
+	cout << "Color: " << myBoiler.color << endl;
+	cout << "Power: " << myBoiler.power << " Watts" << endl;
+	cout << "Volume: " << myBoiler.volume << " liters" << endl;
+	cout << "Heating Temperature: " << myBoiler.heatingTemperature << " °C" << endl;
+
 	return 0;
 }
