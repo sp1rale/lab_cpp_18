f0bda40c4b8fe47e046aa1fe0992f7188dfb1573
sp1rale <dimayaremchyk24@gmail.com>
2023-12-03 19:47:52 +0200

    done

--- main.cpp
+++ main.cpp
@@ -3,11 +3,36 @@
 
 using namespace std;
 
+struct Iron {
+    string brand;
+    string model;
+    string color;
+    int minTemperature;
+    int maxTemperature;
+    bool steamFunction;
+    int power;
+};
+
+
 int main()
 {
-	string name;
-	cout << "Enter your name: ";
-	cin >> name;
-	cout << "Hello, " << name << "!\n";
+    Iron myIron;
+
+    myIron.brand = "ExampleBrand";
+    myIron.model = "Philips";
+    myIron.color = "Black";
+    myIron.minTemperature = 100;
+    myIron.maxTemperature = 200;
+    myIron.steamFunction = true;
+    myIron.power = 1500;
+
+    cout << "Iron Information:" << endl;
+    cout << "Brand: " << myIron.brand << endl;
+    cout << "Model: " << myIron.model << endl;
+    cout << "Color: " << myIron.color << endl;
+    cout << "Temperature Range: " << myIron.minTemperature << " - " << myIron.maxTemperature << " °C" << endl;
+    cout << "Steam Function: " << (myIron.steamFunction ? "Yes" : "No") << endl;
+    cout << "Power: " << myIron.power << " Watts" << endl;
+
 	return 0;
 }
