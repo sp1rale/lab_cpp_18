34ceb8e076b78226c5db5f83e7cdcf3669685cdb
sp1rale <dimayaremchyk24@gmail.com>
2023-12-03 19:24:13 +0200

    d

--- main.cpp
+++ main.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 #include <string>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Hello, " << name << "!\n";
+	return 0;
+}
