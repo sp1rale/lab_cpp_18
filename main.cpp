a947a024cae8410f10ecc93397e847bf05226a62
sp1rale <dimayaremchyk24@gmail.com>
2023-12-03 19:32:56 +0200

    done

--- main.cpp
+++ main.cpp
@@ -3,11 +3,37 @@
 
 using namespace std;
 
+struct WashingMachine {
+	string brand;
+	string color;
+	double height ;
+	double length;
+	double width;
+	int power;
+	int spinSpeed;
+	int  heatingTemperature;
+};
+
 int main()
 {
-	string name;
-	cout << "Enter your name: ";
-	cin >> name;
-	cout << "Hello, " << name << "!\n";
+	WashingMachine myWashingMachine;
+
+	myWashingMachine.brand = "Bosh";
+	myWashingMachine.color = "White";
+	myWashingMachine.width = 60.0;
+	myWashingMachine.length = 80.0;
+	myWashingMachine.height = 100.0;
+	myWashingMachine.power = 1200;
+	myWashingMachine.spinSpeed = 800;
+	myWashingMachine.heatingTemperature = 40;
+
+	cout << "Washing Machine Information:" << endl;
+	cout << "Brand: " << myWashingMachine.brand << endl;
+	cout << "Color: " << myWashingMachine.color << endl;
+	cout << "Dimensions (W x L x H): " << myWashingMachine.width << " x " << myWashingMachine.length << " x " << myWashingMachine.height << endl;
+	cout << "Power: " << myWashingMachine.power << " Watts" << endl;
+	cout << "Spin Speed: " << myWashingMachine.spinSpeed << " RPM" << endl;
+	cout << "Heating Temperature: " << myWashingMachine.heatingTemperature << " °C" << endl;
+
 	return 0;
 }
